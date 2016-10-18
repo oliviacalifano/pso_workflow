1e9fdafa1f0ceb51cf8409e99a0ace8c2ca7dd9b
Olivia Califano <ocalifano@mediamath.com>
2016-10-18 12:21:20 -0400

    removed BATCH supply sources from supply tool

--- tools/supplysource2.0/get_supply.js
+++ tools/supplysource2.0/get_supply.js
@@ -8,6 +8,6 @@ function get_supply_sources()
 	$("#supply_list").empty(); 
 
 
-	aggregate_pages("https://adroit-tools.mediamath.com/t1/api/v2.0/supply_sources?sort_by=name&q=has_display%3D%3D1")
+	aggregate_pages("https://adroit-tools.mediamath.com/t1/api/v2.0/supply_sources?sort_by=name&q=rtb_type%21%3DBATCH%26has_display%3D%3D1")
 		.then(function(xml) { push_entities_to_multiselect(xml, "#supply_list")});
 }
