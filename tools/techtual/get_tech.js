ea8b950a5ca237708922f1a631d060304b4a15a9
Olivia Califano <olivetree13@gmail.com>
2018-02-27 18:44:00 -0500

    new technology tool (replacing old tool)

--- tools/techtual/get_tech.js
+++ tools/techtual/get_tech.js
@@ -1,4 +1,58 @@
 $(function(){
+	var options = [];
+	options.push("<option value=BSER>Browser</option>");
+	options.push("<option value=CSPD>Connection Speed</option>");
+	options.push("<option value=DVCE>Device Type/OS</option>");
+	options.push("<option value=ISPX>ISP</option>");
+	options.push("<option value=INVT>Inventory Type</option>");
+
+	$("#target_values").append(options);
+	$("#target_values").multipleSelect({
+	placeholder: "Choose The Thing",
+	filter: true
+	});	
+});
+
+$("#target_values").change(function(){
+	technology($(this).val());
+});
+
+function technology(ids)
+{
+	var options = [];
+	var aggregate_pages_promises = [];
+	var entities = [];
+
+	console.log("empty strat list..");
+	$("#technology").empty(); 
+
+	if(!ids){
+		console.log("no camps");
+		$("#technology").empty();
+		$("#technology").multipleSelect({
+			placeholder: "Choose Campaigns",
+			filter: true
+		});	
+	}
+
+	else{
+		for(var i=0; i<ids.length; i++){
+			url = "https://adroit-tools.mediamath.com/t1/api/v2.0/target_values?dimension="+ids[i]+"&sort_by=name";
+			console.log(url);
+			aggregate_pages_promises.push(aggregate_pages(url));
+		}
+		Promise.all(aggregate_pages_promises).then(function(xml){
+			push_entities_to_multiselect(xml.join(), "#technology");
+		});
+	}	
+}
+
+
+
+
+
+
+/* $(function(){
 	get_dev();
 	get_inv();
 	get_cspd();
@@ -51,4 +105,4 @@ function get_ispx(){
 
 	aggregate_pages("https://adroit-tools.mediamath.com/t1/api/v2.0/target_values?dimension=ISPX&sort_by=name")
 		.then(function(xml) { push_entities_to_multiselect(xml, "#ispx_list")});
-}
+} */
