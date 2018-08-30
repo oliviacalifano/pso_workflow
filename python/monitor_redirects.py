2342570c5182bd795f915f8feb7b0880f194f460
Olivia Califano <ocalifano@ewr-adsjobs-n1>
2018-08-30 21:39:32 +0000

    updates

--- python/monitor_redirects.py
+++ python/monitor_redirects.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import requests, time, socket, json
+import requests, time, json
 from pprint import pprint
+import socket
 
 import sys
 sys.path.insert(0, "/home/ocalifano")
@@ -36,7 +37,7 @@ class LinkObject():
 		self.FinalURL = ""
 		self.Alert = alert
 		self.CountRedirects = count_redirects
-		self.TotalTime = total_time
+		self.TotalTime = 0
 		self.Attempts = attempts
 		self.DeadLinks = dead_links
 		self.Slow = slow
@@ -49,10 +50,11 @@ class LinkObject():
 			r = requests.get(self.Link, timeout=4)
 			self.History = r.history
 			self.FinalURL = r.url
-			self.TotalTime = r.elapsed
+			#self.TotalTime = r.elapsed
 		except requests.exceptions.Timeout:
 			self.Alert = self.Alert+1
 			self.Slow = self.Slow+1
+			self.TotalTime = 0
 			self.Message.append("request time > 4 sec")
 		except Exception as e: 
 			print(e)
@@ -83,15 +85,16 @@ class LinkObject():
 	
 	#string for email alert
 	def __repr__(self):
-		m = ""
-		for x in self.Message:
-			m = m + "\n Message: " + x
-		return "\n Name: %s \n Link: %s \n Alert: %s \n Redirects: %s \n Time: %s \n Attempts: %s \n Dead: %s \n Slow: %s \n HttpsToHttp: %s" % (self.Name, self.Link, self.Alert, self.CountRedirects, self.TotalTime, self.Attempts, self.DeadLinks, self.Slow, self.HttpsToHttp) + m		
+		alerts_email_body = ""
+		for message in self.Message:
+			alerts_email_body = alerts_email_body + "\n Message: " + "<font color='FF0000'>"+message+"</font>"
+		return "\n Name: %s \n Link: %s \n Redirects: %s \n Time: %s \n Attempts: %s \n Dead: %s \n Slow: %s \n HttpsToHttp: %s" % (self.Name, self.Link, self.CountRedirects, self.TotalTime, self.Attempts, self.DeadLinks, self.Slow, self.HttpsToHttp) + alerts_email_body
 
 	#string for udp stats	
 	def __str__(self):
-		return prefix+"."+str(self.Name)+".redirects"+":"+str(self.CountRedirects)+"|c \n"+prefix+"."+str(self.Name)+ ".attempts" + ":"+str(self.Attempts)+"|c \n" + prefix + "." + str(self.Name) + ".total_time" + ":"+str(self.TotalTime)+"|c \n" + prefix + "." + str(self.Name) + ".error.dead_link" + ":"+str(self.DeadLinks)+"|c \n" + prefix + "." + str(self.Name) + ".error.slow" + ":"+str(self.Slow)+"|c \n" + prefix + "." + str(self.Name) + ".error.https_http" + ":"+str(self.HttpsToHttp)+"|c \n"
-				
+		return prefix+"."+str(self.Name)+".redirects"+":"+str(self.CountRedirects)+"|c\n"+prefix+"."+str(self.Name)+ ".attempts" + ":"+str(self.Attempts)+"|c\n" + prefix + "." + str(self.Name) + ".total_time" + ":"+str(self.TotalTime)+"|c\n" + prefix + "." + str(self.Name) + ".error.dead_link" + ":"+str(self.DeadLinks)+"|c\n" + prefix + "." + str(self.Name) + ".error.slow" + ":"+str(self.Slow)+"|c\n" + prefix + "." + str(self.Name) + ".error.https_http" + ":"+str(self.HttpsToHttp)+"|c\n"
+		#prefix+"."+str(self.Name)+".redirects"+":"+str(self.CountRedirects)+"|c \n"+prefix+"."+str(self.Name)+ ".attempts" + ":"+str(self.Attempts)+"|c \n" + prefix + "." + str(self.Name) + ".total_time" + ":"+str(self.TotalTime)+"|c \n" + prefix + "." + str(self.Name) + ".error.dead_link" + ":"+str(self.DeadLinks)+"|c \n" + prefix + "." + str(self.Name) + ".error.slow" + ":"+str(self.Slow)+"|c \n" + prefix + "." + str(self.Name) + ".error.https_http" + ":"+str(self.HttpsToHttp)+"|c \n"
+
 def send_email(text):
 	mytext = "<br />".join(text.split("\n"))
 	username = 'ocalifano@mediamath.com'
@@ -110,21 +113,25 @@ def send_email(text):
 	server.quit()
 
 def send_udp_packet(text):
-	UDP_IP = ""
+	UDP_IP = 'ewr-stats-x1.mediamath.com'
 	UDP_PORT = 57475
 	
-	print("UDP target IP:", UDP_IP)
-	print("UDP target port:", UDP_PORT)
-	print("message:", text)
+	#print("UDP target IP:", UDP_IP)
+	#print("UDP target port:", UDP_PORT)
+	#print("message:", text)
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	sock.sendto(bytes(text, "utf-8"), (UDP_IP, UDP_PORT))
+	print(sock.recv(100))
+	# sock.sendto(text.encode(),(UDP_IP, UDP_PORT))
 	
 	
 def read_in_links_from_github():
 	#retrieve the current partner list from github
-	response = requests.get('https://raw.githubusercontent.com/ocalifano/core-mt3/master/compat/config/sync/partners.json?token=AOohJlVQE3dulnb-VnECxyEOiVwXZO51ks5biHDIwA%3D%3D', auth=(git_user, git_password))
-	data = json.loads(response.text)
+	#response = requests.get('https://raw.githubusercontent.com/ocalifano/core-mt3/master/compat/config/sync/partners.json?token=AOohJlVQE3dulnb-VnECxyEOiVwXZO51ks5biHDIwA%3D%3D', auth=(git_user, git_password))
+	#data = json.loads(response.text)
+	with open('partners.json') as data_file:
+		data = json.load(data_file)		
 			
 	for partner in data["partners"]:
 		try: 
@@ -152,8 +159,8 @@ def main():
 	read_in_links_from_github()
 
 	#instantiate https links as objects
-	for i in range(len(https)):
-		newLink = LinkObject(common_name_https[i], https[i], 0, 0, 0, 0, 0, 0, 0)
+	for i in range(len(http)):
+		newLink = LinkObject(common_name_http[i], http[i], 0, 0, 0, 0, 0, 0, 0)
 		newLink.Attempts = newLink.Attempts + 1
 		newLink.Timer()
 		newLink.HttpsToHttpCheck()
@@ -173,12 +180,12 @@ def main():
 	
 	#send stats to udp server
 	if udp != "":
-		print(udp)
-		#send_udp_packet(udp)
+		#print(udp)
+		send_udp_packet(udp)
 	
 	#send email with alerts
-	if email != "":
-		send_email(email)
+	# if email != "":
+		# send_email(email)
 	
 if __name__== "__main__":
   main()
